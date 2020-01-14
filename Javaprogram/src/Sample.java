a26f652b51bb97b2e992c9091769ed50724db24c
HP <HP@192.168.0.122>
2020-01-14 13:01:40 +0530

    hello

---
+++ Javaprogram/src/Sample.java
@@ -0,0 +1,21 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+      int grade='2';
+      switch(grade)
+      {
+      case 1:System.out.println("FCD");
+      break;
+      case 2:System.out.println("Fd");
+      break;
+      case 3:System.out.println("SC");
+      break;
+      case 4:System.out.println("Fail");
+      break;
+      default:System.out.println("invalid");
+      
+      }
+	}
+}
+      
